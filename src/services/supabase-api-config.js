4ef5de69820dc343356d91aac6ca11b67380df52
ruslan-at-615135307338 <r.gusseinov@mail.ru>
2023-07-13 12:53:07 +0600

    add env config for database credentials

--- src/services/supabase-api-config.js
+++ src/services/supabase-api-config.js
@@ -1,7 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mhvaudkjbregorhvobit.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1odmF1ZGtqYnJlZ29yaHZvYml0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkwMDc0ODksImV4cCI6MjAwNDU4MzQ4OX0.-E0sRGUA7ELr3Ofqz1d8Hfl7t7AKI04Qz0LjE3wGSYU';
+const defaultEnvConfig = {
+  VITE_SUPABASE_URL: '',
+  VITE_APP_ANON_KEY: ''
+}
 
+const envConfig = {...defaultEnvConfig, ...import.meta.env }
+
+const supabaseUrl = envConfig.VITE_SUPABASE_URL;
+const supabaseAnonKey = envConfig.VITE_ANON_KEY;
+ 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
